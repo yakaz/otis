d30761c48bbde0adafefc6353a5ab2b35912d3fc
Jean-Sébastien Pédron <jean-sebastien@yakaz.com>
2012-07-04 15:27:38 +0200

    Allow one to specify initial state when calling rewrite engine

--- priv/template/otis_reqrw_engine.erl
+++ priv/template/otis_reqrw_engine.erl
@@ -20,8 +20,10 @@
 -export([
     uri/1,
     uri/2,
+    uri/3,
     request/1,
     request/2,
+    request/3,
     eval/1,
     call_rule/2,
     conf/0,
@@ -36,28 +38,34 @@ uri(URI) ->
     uri(URI, []).
 
 uri(URI, Headers) ->
-    State = request2(URI, Headers),
+    uri(URI, Headers, #state{}).
+
+uri(URI, Headers, State) ->
+    State1 = request2(URI, Headers, State),
     Var = otis_var:parse("$(URI)"),
-    {_, Result} = otis_var:expand(State, Var),
+    {_, Result} = otis_var:expand(State1, Var),
     lists:flatten(Result).
 
 request(URI) ->
     request(URI, []).
 
 request(URI, Headers) ->
-    State = request2(URI, Headers),
+    request(URI, Headers, #state{}).
+
+request(URI, Headers, State) ->
+    State1 = request2(URI, Headers, State),
     Var = otis_var:parse("$(RESULT)"),
-    {_, Result} = otis_var:expand(State, Var),
+    {_, Result} = otis_var:expand(State1, Var),
     lists:flatten(Result).
 
-request2(URI, Headers) ->
+request2(URI, Headers, State) ->
     {Scheme, Host, Port, Path, Query} = otis_utils:parse_uri(URI),
     Server_IP = otis_utils:server_ip(Host),
     Headers1  = [
       {string:to_lower(H), V, undefined, undefined}
       || {H, V} <- Headers
     ],
-    State = #state{
+    State1 = State#state{
       server_name = Host,
       server_ip   = Server_IP,
       server_port = Port,
@@ -67,14 +75,14 @@ request2(URI, Headers) ->
       headers     = Headers1,
       query_str   = Query
     },
-    State1 = case otis_var:get_header(State, "host") of
+    State2 = case otis_var:get_header(State1, "host") of
         {_, undefined} ->
             Host1 = otis_utils:format_host(Scheme, Host, Port),
-            otis_var:set_header(State, "host", Host1);
+            otis_var:set_header(State1, "host", Host1);
         _ ->
-            State
+            State1
     end,
-    eval(State1).
+    eval(State2).
 
 eval(State) ->
     ?FIRST_RULE(State).
